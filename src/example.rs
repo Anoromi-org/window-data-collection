60bd9b58dd88fec1643178124a30faeeb500e640
Anoromi <and.zagorulko@gmail.com>
2025-04-21 14:07:36 +0300

    test

--- src/example.rs
+++ src/example.rs
@@ -1,6 +1,8 @@
 // This runs daemon on windows without creating a console. Disable during development to see
 // stdout.
 
+use std::{thread::sleep, time::Duration};
+
 use anyhow::Result;
 use whatawhat::window_api::{GenericWindowManager, WindowManager};
 
@@ -11,5 +13,6 @@ async fn main() -> Result<()> {
         println!("Window data {:?}", manager.get_active_window_data().await);
         println!("Idle time{:?}", manager.get_idle_time().await);
         println!();
+        sleep(Duration::from_secs(1));
     }
 }
