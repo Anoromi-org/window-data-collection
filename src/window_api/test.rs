c5350268424cf82bae048ec1848edef37d77d4c5
Anoromi <and.zagorulko@gmail.com>
2025-04-22 12:38:49 +0300

    test

--- src/window_api/test.rs
+++ src/window_api/test.rs
@@ -346,7 +346,6 @@ mod generated {
         }
         #[inline]
         fn from_id(conn: &Connection, id: ObjectId) -> Result<Self, InvalidId> {
-          dbg!(id.interface(), Self::interface(), !id.is_null());
           if !same_interface(id.interface(), Self::interface()) && !id.is_null() {
             return Err(InvalidId);
           }
