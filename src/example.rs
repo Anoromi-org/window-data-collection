57a31b1356ccc5dbac7041b5b547b2ec6b0d1abd
Anoromi <and.zagorulko@gmail.com>
2025-04-22 09:28:11 +0300

    test

--- src/example.rs
+++ src/example.rs
@@ -1,13 +1,15 @@
 // This runs daemon on windows without creating a console. Disable during development to see
 // stdout.
 
-use std::{thread::sleep, time::Duration};
+use std::{path::Path, thread::sleep, time::Duration};
 
 use anyhow::Result;
-use whatawhat::window_api::{GenericWindowManager, WindowManager};
+use tracing::level_filters::LevelFilter;
+use whatawhat::{utils::logging::enable_logging, window_api::{GenericWindowManager, WindowManager}};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    enable_logging("example", "logs".as_ref(), Some(LevelFilter::TRACE), true)?;
     let mut manager = GenericWindowManager::new().await?;
     loop {
         println!("Window data {:?}", manager.get_active_window_data().await);
