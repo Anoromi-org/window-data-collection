2574e9fe75eb0a72becf3b20b102110db048969e
Anoromi <and.zagorulko@gmail.com>
2025-04-23 12:41:37 +0300

    test

--- src/window_api/macos.rs
+++ src/window_api/macos.rs
@@ -12,6 +12,8 @@ set windowTitle to ""
 tell application "System Events"
     set frontApp to first application process whose frontmost is true
     set frontAppName to name of frontApp
+    set processID to unix id of frontApp -- Get the process ID
+
     tell process frontAppName
         try
             tell (1st window whose value of attribute "AXMain" is true)
@@ -22,23 +24,26 @@ tell application "System Events"
 end tell
 
 return frontAppName & "
-" & windowTitle
+" & windowTitle & "
+" & processID
 "#;
 
+#[derive(Default)]
 pub struct MacosManger {}
 
 impl MacosManger {
   pub fn new() -> Self {
-    Self {}
+    Default::default()
   }
 }
+
 
 #[async_trait]
 impl ActiveWindowManager for MacosManger {
   async fn get_active_window_data(&mut self) -> Result<ActiveWindowData> {
     let hello = NSAppleScript::alloc();
     unsafe {
-      let help = NSAppleScript::initWithSource(hello, ns_string!(SOURCE)).expect("TODO");
+      let help = NSAppleScript::initWithSource(hello, ns_string!(SOURCE)).expect("Source script is expected to be always correct");
       let mut err: Option<Retained<NSDictionary<NSString, AnyObject>>> = None;
       let descriptor = help.executeAndReturnError(Some(&mut err));
       dbg!(descriptor.stringValue());
