ba124dc7fbf459e26fed62a56729d5f348620beb
Anoromi <and.zagorulko@gmail.com>
2025-04-23 12:51:43 +0300

    test

--- src/window_api/macos.rs
+++ src/window_api/macos.rs
@@ -3,6 +3,8 @@ use async_trait::async_trait;
 use objc2::{rc::Retained, runtime::AnyObject, AnyThread};
 use objc2_foundation::{ns_string, NSAppleScript, NSDictionary, NSString};
 
+use crate::window_api::process_name;
+
 use super::{ActiveWindowData, ActiveWindowManager};
 
 const SOURCE: &str = r#"
@@ -37,20 +39,28 @@ impl MacosManger {
   }
 }
 
-
 #[async_trait]
 impl ActiveWindowManager for MacosManger {
   async fn get_active_window_data(&mut self) -> Result<ActiveWindowData> {
     let hello = NSAppleScript::alloc();
-    unsafe {
-      let help = NSAppleScript::initWithSource(hello, ns_string!(SOURCE)).expect("Source script is expected to be always correct");
-      let mut err: Option<Retained<NSDictionary<NSString, AnyObject>>> = None;
-      let descriptor = help.executeAndReturnError(Some(&mut err));
-      dbg!(descriptor.stringValue());
-      println!("{}", descriptor.stringValue().unwrap());
+    let help = unsafe { NSAppleScript::initWithSource(hello, ns_string!(SOURCE)) }
+      .expect("Source script is expected to be always correct");
+    let mut err: Option<Retained<NSDictionary<NSString, AnyObject>>> = None;
+    let descriptor = unsafe { help.executeAndReturnError(Some(&mut err)) };
 
-    }
+    let data = unsafe { descriptor.stringValue() }
+      .ok_or_else(|| anyhow!("Source script is expected to return string value but got {:?}", descriptor))?
+      .to_string();
+    let values = data.lines().collect::<Vec<_>>();
+    let app_id = values[0];
+    let window_title = values[1];
+    let process_id = values[2].parse::<u32>()?;
+    let process_name = process_name::get_process_name(process_id)?;
 
-    Err(anyhow!("Still developing"))
+    return Ok(ActiveWindowData {
+      window_title: window_title.to_string().into(),
+      process_name: process_name.map(Into::into),
+      app_id: Some(app_id.to_string().into()),
+    });
   }
 }
--- src/window_api/process_name.rs
+++ src/window_api/process_name.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use sysinfo::Pid;
 
 
-pub(crate) fn get(id: u32) -> Result<Option<String>> {
+pub(crate) fn get_process_name(id: u32) -> Result<Option<String>> {
     let system = sysinfo::System::new_all();
     let Some(process) = system.process(Pid::from_u32(id)) else {
         return Ok(None);
